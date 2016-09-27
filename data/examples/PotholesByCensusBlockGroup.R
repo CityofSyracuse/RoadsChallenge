044995ca5822d32a587bc8a1de351203be8f7aa7
samedelstein <sam.i.edelstein@gmail.com>
2016-09-27 10:58:20 -0400

    added code

---
+++ data/examples/PotholesByCensusBlockGroup.R
@@ -0,0 +1,22 @@
+library(rgdal)
+library(raster)
+library(dplyr)
+library(stringr)
+library(GISTools)
+
+url <- "https://cityofsyracuse.github.io/RoadsChallenge/data/potholes.csv"
+potholes <- read.csv(url, stringsAsFactors = FALSE)
+head(potholes)
+censusBlocks <- readOGR(".",'block_groups_2010_city')
+censusBlocks <- spTransform(censusBlocks, CRS("+proj=longlat +datum=WGS84 +ellps=GRS80 +towgs84=0,0,0"))
+head(potholes)
+head(streetspotholes)
+
+points <- potholes[,c("Longitude", "Latitude")]
+spdf <- SpatialPointsDataFrame(coords = points, data = potholes,
+                               proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=GRS80 +towgs84=0,0,0"))
+
+n.censusblocks <- poly.counts(spdf, censusBlocks)
+shades = auto.shading(n.censusblocks,cols=brewer.pal(5,'Blues'))
+choropleth(censusBlocks, n.censusblocks/poly.areas(censusBlocks), shading=auto.shading(n.censusblocks/poly.areas(censusBlocks)))
+
